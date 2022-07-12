137a3eba6249a66e578ef5f28454f18e9174e8df
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2022-07-12 19:27:03 +0100

    Add files via upload

---
+++ DomainSuperhero/Interfaces/ISuperheroRepository.cs
@@ -0,0 +1,19 @@
+﻿using DomainSuperhero.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainSuperhero.Interfaces
+{
+    public interface ISuperheroRepository
+    {
+        Task<IEnumerable<SuperheroModel>> GetAllSuperherores();
+        Task<SuperheroModel> GetOneSuperhero(int id);
+        Task<SuperheroModel> InsertSuperhero(SuperheroModel model);
+        Task<SuperheroModel> UpdateSuperhero(SuperheroModel model);
+        Task<SuperheroModel> DeleteSuperhero(int id);
+    }
+}
---
+++ DomainSuperhero/Models/SuperheroModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainSuperhero.Models
+{
+    public class SuperheroModel
+    {
+        public int SuperheroID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string SuperheroName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
