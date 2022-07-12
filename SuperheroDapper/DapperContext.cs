e35fbf297f3230733848a0579773ae329852e268
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2022-07-12 19:20:03 +0100

    Add files via upload

---
+++ SuperheroDapper/DapperContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroDapper
+{
+    public class DapperContext 
+    {
+        public IConfiguration _config;
+        private readonly string _connString;
+
+        public DapperContext(IConfiguration context)
+        {
+            _config = context;
+            _connString = _config.GetConnectionString("Default");
+        }
+
+        public IDbConnection GetConnectionString()
+            => new SqlConnection(_connString);
+
+
+
+    }
+}
